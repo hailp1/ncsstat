81472e33dd0e26829716e8d0dba98629c20adff6
hailp1 <phuchai.le@gmail.com>
2026-01-28 15:54:10 +0700

    Cleanup: remove lingering utils/supabase directory

--- utils/supabase/promote_admin.sql
+++
@@ -1,7 +0,0 @@
--- Promote specific user to admin
-update public.profiles
-set role = 'admin'
-where email = 'foreverlove3004@gmail.com';
-
--- Verify the update
-select email, role from public.profiles where email = 'foreverlove3004@gmail.com';
